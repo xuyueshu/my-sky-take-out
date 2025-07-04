0ed8edcf1370030bf403286e39ed6bbfb4487e2b
xuyueshu <980043299@qq.com>
2025-07-05 00:50:07 +0800

    配置类WebMvcConfiguration，注册web层相关组件

--- sky-server/src/main/java/com/zhiyou/skyserver/interceptor/JwtTokenAdminInterceptor.java
+++ sky-server/src/main/java/com/zhiyou/skyserver/interceptor/JwtTokenAdminInterceptor.java
@@ -14,30 +14,29 @@ import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 @Component
 @Slf4j
-public class JwtTokenUserInterceptor implements HandlerInterceptor {
+public class JwtTokenAdminInterceptor implements HandlerInterceptor {
 
     @Autowired
     private JwtProperties jwtProperties;
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        // 如果不是访问动态方法，直接放行
-        if (!(handler instanceof HandlerMethod)){
+        // 如果拦截到的不是动态方法，直接放行
+        if(! (handler instanceof HandlerMethod)){
             return true;
         }
+        // 从请求头中获取token
+        String token = request.getHeader(jwtProperties.getAdminTokenName());
+        log.info("获取到admin的token：{}",token);
 
-        // 获取token
-        String token = request.getHeader(jwtProperties.getUserTokenName());
-        log.info("获取到用户token：{}",token);
         try {
-            // 解析token
-            Claims claims = JwtUtil.parseJWT(jwtProperties.getUserSecretKey(), token);
-            Long userId = Long.valueOf(claims.get(JwtClaimsConstant.USER_ID).toString());
-            // 将id存储到ThreadLocal中
-            BaseContext.setCurrentId(userId);
-            // 放行
+            Claims claims = JwtUtil.parseJWT(jwtProperties.getAdminSecretKey(), token);
+            Long empId = Long.valueOf(claims.get(JwtClaimsConstant.EMP_ID).toString());
+            log.info("当前员工的id为：{}",empId);
+            BaseContext.setCurrentId(empId);
             return true;
-        } catch (Exception e) {
-            log.error("jwt解析失败:{}",e.getMessage());
+        } catch (Exception e){
+            log.error("令牌解析失败：{}",e.getMessage());
             response.setStatus(401);
         }
         return false;
