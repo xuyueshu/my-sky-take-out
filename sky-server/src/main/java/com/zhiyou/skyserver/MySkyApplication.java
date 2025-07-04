c528c25b22d0b8011d59baf63ae824e7dd893f3d
xuyueshu <980043299@qq.com>
2025-07-04 23:26:54 +0800

    创建和解析jwt令牌，并创建测试方法

--- sky-server/src/main/java/com/zhiyou/skyserver/MySkyApplication.java
+++ sky-server/src/main/java/com/zhiyou/skyserver/MySkyApplication.java
@@ -5,6 +5,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -14,6 +15,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableCaching                  //开启缓存注解功能
 @MapperScan("com.zhiyou.mapper")   //指定扫描mapper
 @EnableTransactionManagement    //开启注解方式的事务管理
+@ComponentScan("com.zhiyou") // 指定注册要扫描的包，不指定会默认扫描当前包及子包
 public class MySkyApplication {
 
     public static void main(String[] args) {
---
+++ sky-server/src/main/java/com/zhiyou/skyserver/interceptor/JwtTokenUserInterceptor.java
@@ -0,0 +1,13 @@
+package com.zhiyou.skyserver.interceptor;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+@Component
+public class JwtTokenUserInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        return HandlerInterceptor.super.preHandle(request, response, handler);
+    }
+}
